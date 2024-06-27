1eb50408554a39b226bac499fe239e909b1094ce
Dell <Dell@DESKTOP-70K75Q4>
2024-06-27 13:32:57 +0700

    Add project files.

---
+++ SportsStore/Controllers/CuaHangXeDap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class CuaHangXeDap : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SportsStore/Controllers/TrangChu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class TrangChu : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
